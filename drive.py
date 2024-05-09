fec94a63aa365972fd27798a8e52e4719e0dac56
jghughesai <162188970+jghughesai@users.noreply.github.com>
2024-05-09 16:00:44 -0400

    Query for files with the same name as activity names

---
+++ drive.py
@@ -0,0 +1,71 @@
+import os
+import os.path
+import io
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaFileUpload
+from googleapiclient.http import MediaIoBaseDownload
+
+def get_drive_files(creds, file_names):
+  try:
+    service = build("drive", "v3", credentials=creds)
+    folder_response = service.files().list(
+    q="name='file test' and mimeType='application/vnd.google-apps.folder'",
+    spaces="drive",
+    fields="files(id, name)"
+    ).execute()
+
+    folder_response_result = folder_response.get("files", [])
+    folder_id = folder_response_result[0].get("id")
+
+    for file_name in file_names:
+        print(f"file_name: {file_name}")
+        file_response = service.files().list(
+        q = f"name='{file_name}' and '{folder_id}' in parents",
+        fields="files(id, name)"
+        ).execute()
+
+        print(f"\nfile response: {file_response}\n")
+    return "hi"
+
+  except HttpError as error:
+    # TODO(developer) - Handle errors from drive API.
+    print(f"An error occurred getting the list of files: {error}")
+  
+#   files_dict = {
+#     "files": [
+#     ]
+#   }
+
+#   try:
+#     files = file_response["files"]
+#     for file in files:
+#       file_name = file.get("name")
+#       print(f"file_name: {file_name}")
+#       file_id = file.get("id")
+#       print(file_id)
+#       request = service.files().get_media(fileId=file_id)
+#       print("request works")
+#       file_buffer = io.BytesIO()
+#       downloader = MediaIoBaseDownload(file_buffer, request)
+#       done = False
+#       while done is False:
+#         status, done = downloader.next_chunk()
+#         print(f"Download {int(status.progress() * 100)}.")
+#       file_str = str(file_buffer.getvalue())
+#       print(file_str)
+
+#       files_dict["files"].append({
+#         "name": file_name,
+#         "id": file_id,
+#         "file_content": file_str
+#       })
+#     return files_dict
+  
+#   except HttpError as error:
+#     # TODO(developer) - Handle errors from drive API.
+#     print(f"An error occurred downloading the file(s): {error}")
