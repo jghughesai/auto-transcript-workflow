c21df9d0cf22e647730797afa444fc3d011635b9
jghughesai <162188970+jghughesai@users.noreply.github.com>
2024-05-14 15:03:46 -0400

    Add display response function for appending step cards to side bar

--- static/script.js
+++ static/script.js
@@ -1,4 +1,5 @@
 const runBtn = document.querySelector("button");
+const sideBarContainer = document.getElementById("sideBarContainer");
 
 runBtn.addEventListener("click", () => {
     console.log("Hi");
@@ -7,9 +8,32 @@ runBtn.addEventListener("click", () => {
             if (!response.ok) {
                 throw new Error("Network response was not ok");
             }
-            console.log('Request successful with no data to return');
+            return response.json();
+        })
+        .then(data => {
+            console.log("Server Response: ", data)
+            displayResponse(data)
         })
         .catch(error => {
             console.error("Error:", error);
         })
 })
+
+function displayResponse(data) {
+    if (data === "success") {
+        const sideBarItem = document.createElement("div");
+        sideBarItem.classList.add("step-card");
+        sideBarContainer.append(sideBarItem);
+        const sideBarItemPara = document.createElement("p");
+        sideBarItemPara.textContent = "Successfully retrieved the AI generated summary of your transcript and uploaded it to your outputs folder."
+        sideBarItem.append(sideBarItemPara);
+    }
+    else {
+        const sideBarItem = document.createElement("div");
+        sideBarItem.classList.add("step-card");
+        sideBarContainer.append(sideBarItem);
+        const sideBarItemPara = document.createElement("p");
+        sideBarItemPara.textContent = "No Google Drive activity detected. Perhaps you forgot to add a transcript to the inputs folder?"
+        sideBarItem.append(sideBarItemPara);
+    }
+}
